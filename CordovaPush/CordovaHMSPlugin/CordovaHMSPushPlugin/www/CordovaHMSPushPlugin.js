7c35f685dcdcfe0c245ded88bf19a21fab00d062
Renier.Forster <renierf@tauinventions.com>
2020-02-07 11:44:08 +0200

    Updated Push Plugin and added the Location Plugin

--- CordovaPush/CordovaHMSPlugin/CordovaHMSPushPlugin/www/CordovaHMSPushPlugin.js
+++ CordovaPush/CordovaHMSPlugin/CordovaHMSPushPlugin/www/CordovaHMSPushPlugin.js
@@ -2,4 +2,8 @@ var exec = require('cordova/exec');
 
 exports.getToken = function (arg0, success, error) {
     exec(success, error, "CordovaHMSPushPlugin", "getToken", [arg0]);
-}
+};
+
+exports.getMessageCallback = function (arg0, success, error) {
+    exec(success, error, "CordovaHMSPushPlugin", "getMessageCallback", [arg0]);
+};
